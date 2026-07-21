9744971fac9c2e589c723bfc5827e07f4ad6e9e1
daniu <hfzdania55@gmail.com>
2026-07-21 09:48:32 +0700

    remove custom distDir

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'dist',
   images: {
     unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
